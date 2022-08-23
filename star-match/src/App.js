30539e21c97f7b8998bc1d1a8fdfe04eb43ff3c6
Martin Bedoya <83382332+josemartin357@users.noreply.github.com>
2022-08-23 16:40:33 -0700

    Added initial code and design layout

---
+++ star-match/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StarMatch from './components/StarMatch';
+
+function App() {
+  return (
+    <div>
+      <StarMatch/>
+    </div>
+  );
+}
+
+export default App;
