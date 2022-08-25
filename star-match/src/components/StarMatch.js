78d6d63fdfd75754e29995d6a848ce9e723a40bf
Martin Bedoya <83382332+josemartin357@users.noreply.github.com>
2022-08-25 07:21:48 -0700

    Update 5: updated final game over and time limit components

--- star-match/src/components/StarMatch.js
+++ star-match/src/components/StarMatch.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 
 // number component
 const PlayNumber = props => (
@@ -23,118 +23,168 @@ const StarsDisplay = props => (
 </Fragment>
 );
 
+// game over component
+const PlayAgain = props => (
+	<div className="game-done">
+  	<div 
+    	className="message"
+      style={{ color: props.gameStatus === 'lost' ? 'red' : 'green'}}
+    >
+  	  {props.gameStatus === 'lost' ? 'Game Over' : 'Nice'}
+  	</div>
+	  <button onClick={props.onClick}>Play Again</button>
+	</div>
+);
+
+// time limit component
+const useGameState = timeLimit => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(20);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => setSecondsLeft(secondsLeft - 1), 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+
+  const setGameState = (newCandidateNums) => {
+    if (utils.sum(newCandidateNums) !== stars) {
+			setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
+  return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+};
+
+
+//  game component
+const Game = props => {
+  // making stars a state element when they have a value that will change (tip)
+  const {
+    stars,
+    availableNums,
+    candidateNums,
+    secondsLeft,
+    setGameState,
+  } = useGameState();
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+  const gameStatus = availableNums.length === 0 
+  	? 'won'
+    : secondsLeft === 0 ? 'lost' : 'active'
+
+  // function to pass number status
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
+      return 'used';
+    }
+
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+
+    return 'available';
+  };
+
+  // funtion to define what will happen with every number click
+  const onNumberClick = (number, currentStatus) => {
+    // currentStatus => newStatus
+    if (currentStatus === 'used' || secondsLeft === 0) {
+      return;
+    }
+
+    // here we handle candidate numbers and wrong numbers
+    const newCandidateNums =
+      currentStatus === 'available'
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    setGameState(newCandidateNums);
+  };
+
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {gameStatus !== 'active' ? (
+          	<PlayAgain onClick={props.startNewGame} gameStatus={gameStatus} />
+          ) : (
+          	<StarsDisplay count={stars} />
+          )}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number => (
+            <PlayNumber
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+              onClick={onNumberClick}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
+    </div>
+  );
+};
 
 const StarMatch = () => {
-    // making stars a state element when they have a value that will change (tip)
-    const [stars, setStars] = useState(utils.random(1, 9));  
-    const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-    const [candidateNums, setCandidateNums] = useState([]);
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+  
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
 
-    const candidatesAreWrong = utils.sum(candidateNums) > stars;
+// Math science
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
 
-    // function to pass number status
-    const numberStatus = (number) => {
-      if (!availableNums.includes(number)) {
-        return 'used';
-      }
-      if (candidateNums.includes(number)) {
-        return candidatesAreWrong ? 'wrong': 'candidate';
-      }
-      return 'available';
-    };
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
 
-    // funtion to define what will happen with every number click
-    const onNumberClick = (number, currentStatus) => {
-      // currentStatus => newStatus
-      if (currentStatus === 'used') {
-        return;
-      }
-  
-      const newCandidateNums =
-        currentStatus === 'available'
-          ? candidateNums.concat(number)
-          : candidateNums.filter(cn => cn !== number);
-  
-      if (utils.sum(newCandidateNums) !== stars) {
-        setCandidateNums(newCandidateNums);
-      } else {
-        const newAvailableNums = availableNums.filter(
-          n => !newCandidateNums.includes(n)
-        );
-        setStars(utils.randomSumIn(newAvailableNums, 9));
-        setAvailableNums(newAvailableNums);
-        setCandidateNums([]);
-      }
-    };
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
 
-
-    return (
-      <div className="game">
-        <div className="help">
-          Pick 1 or more numbers that sum to the number of stars
-        </div>
-        <div className="body">
-          <div className="left">
-            <StarsDisplay 
-            count={stars}
-            />
-          </div>
-          <div className="right">
-          {/* TIP: with components, think: 1- UI logic to describe state, 2- App logic to change state */}
-          {/* using dynamic expression/mapping to get numbers populated */}
-            {utils.range(1, 9).map(number => 
-              <PlayNumber 
-              key={number} 
-              number={number} 
-              status={numberStatus(number)}
-              onClick={onNumberClick}
-              />
-            )}
-          </div>
-        </div>
-        <div className="timer">Time Remaining: 10</div>
-      </div>
-    );
-  };
-  
-  // Color Theme
-  const colors = {
-    available: 'lightgray',
-    used: 'lightgreen',
-    wrong: 'lightcoral',
-    candidate: 'deepskyblue',
-  };
-  
-  // Math science
-  const utils = {
-    // Sum an array
-    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-  
-    // create an array of numbers between min and max (edges included)
-    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-  
-    // pick a random number between min and max (edges included)
-    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-  
-    // Given an array of numbers and a max...
-    // Pick a random sum (< max) from the set of all available sums in arr
-    randomSumIn: (arr, max) => {
-      const sets = [[]];
-      const sums = [];
-      for (let i = 0; i < arr.length; i++) {
-        for (let j = 0, len = sets.length; j < len; j++) {
-          const candidateSet = sets[j].concat(arr[i]);
-          const candidateSum = utils.sum(candidateSet);
-          if (candidateSum <= max) {
-            sets.push(candidateSet);
-            sums.push(candidateSum);
-          }
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
         }
       }
-      return sums[utils.random(0, sums.length - 1)];
-    },
-  };
-  
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+
 export default StarMatch;
 
 // TIPS
